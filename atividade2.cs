0ba539c96d38538efed6fbfca791ec244297f6bd
eric <erikinhobs@gmail.com>
2023-04-05 13:31:33 -0300

    .

---
+++ atividade2.cs
@@ -0,0 +1,14 @@
+using System;
+
+class program{
+
+    static void Main(){
+            
+        string nome;
+
+        Console.WriteLine("Digite seu nome:");
+        nome = Console.ReadLine();
+        Console.WriteLine("Seja bem vindo " + nome);
+
+    }
+}
